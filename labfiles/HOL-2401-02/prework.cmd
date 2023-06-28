919031d96193e0b564d1c1e1335d9af8902c26e9
Bengt Grønås <bgronas@users.noreply.github.com>
2023-06-28 12:36:09 +0200

    Create prework.cmd
    A file that is supposed to upload and set the correct rights on script files needed for labs

---
+++ labfiles/HOL-2401-02/prework.cmd
@@ -0,0 +1,37 @@
+
+@echo off
+
+setlocal enabledelayedexpansion
+
+:: Define the array of server IP addresses
+set "server_ips=192.168.110.120 192.168.110.121 192.168.110.122"
+
+:: Define the login name
+set "username=root"
+
+:: Set the local and remote directories
+set "local_directory=C:\local\files"
+set "remote_directory=/root/files"
+
+:: Define the array of file extensions
+set "file_extensions=sh cmd exe"
+
+cls
+
+:: Iterate over each server IP address
+for %%A in (%server_ips%) do (
+    set "server_ip=%%A"
+
+    echo Processing server !server_ip! with username %username%
+
+    :: Iterate over each file extension
+    for %%B in (%file_extensions%) do (
+        set "file_extension=%%B"
+
+        :: Copy files from local to remote server
+        pscp -r "%local_directory%\*.!file_extension!" %username%@!server_ip!:%remote_directory% --batch
+
+        :: Set file permissions on the remote server
+        plink -ssh -batch %username%@!server_ip! "chmod 755 %remote_directory%\*.!file_extension!"
+    )
+)
