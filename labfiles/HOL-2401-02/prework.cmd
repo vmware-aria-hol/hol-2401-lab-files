062052d410135c33f1f56dac7f0765b0588306c7
Bengt Grønås <bgronas@users.noreply.github.com>
2023-06-28 14:40:13 +0200

    Update prework.cmd

--- labfiles/HOL-2401-02/prework.cmd
+++ labfiles/HOL-2401-02/prework.cmd
@@ -1,8 +1,8 @@
 @echo off
 setlocal enabledelayedexpansion
 
-:: Define the array of server IP addresses
-set "server_ips=192.168.110.120 192.168.110.122"
+:: Define the server IP address
+set "server_ip=192.168.110.120"
 
 :: Define the login name
 set "username=root"
@@ -11,25 +11,15 @@ set "username=root"
 set "local_directory=C:\hol-2401-lab-files\labfiles\HOL-2401-02"
 set "remote_directory=/root"
 
-:: Define the array of file extensions
-set "file_extensions=sh"
+:: Define the file extension
+set "file_extension=sh"
 
 cls
 
-:: Iterate over each server IP address
-for %%A in (%server_ips%) do (
-    set "server_ip=%%A"
+echo Processing server %server_ip% with username %username%
 
-    echo Processing server !server_ip! with username %username%
+:: Copy files from local to remote server
+pscp -r "%local_directory%\*.%file_extension%" %username%@%server_ip%:%remote_directory% --batch
 
-    :: Iterate over each file extension
-    for %%B in (%file_extensions%) do (
-        set "file_extension=%%B"
-
-        :: Copy files from local to remote server
-        pscp -r "%local_directory%\*.!file_extension!" %username%@!server_ip!:%remote_directory% --batch
-
-        :: Set file permissions on the remote server
-        plink -ssh -batch %username%@!server_ip! "chmod 755 %remote_directory%\*.!file_extension!"
-    )
-)
+:: Set file permissions on the remote server
+plink -ssh -batch %username%@%server_ip% "chmod 755 %remote_directory%\*.%file_extension%"
