eaf02afee763e32ffc6a0ee080a0c557af6516dd
Bengt Grønås <bgronas@users.noreply.github.com>
2023-07-13 13:02:40 +0200

    Update linux_err_count.sh

--- labfiles/HOL-2401-02/linux_err_count.sh
+++ labfiles/HOL-2401-02/linux_err_count.sh
@@ -2,6 +2,6 @@
 
 # Number of SYSLOG errors in the last 7 days
 
-error_count=$(grep -i "error" /var/log/syslog | grep "$(date --date='7 days ago' '+%b %e')" | wc -l)
+error_count=$(grep -i "error" /var/log/syslog | wc -l)
 
 echo $error_count
