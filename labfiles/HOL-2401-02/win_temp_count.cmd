6fd54bed79f1c914076c4df1472576ea3710506d
Bengt Grønås <bgronas@users.noreply.github.com>
2023-07-13 05:54:37 +0200

    Update win_temp_count.cmd
    count files in c:\temp

--- labfiles/HOL-2401-02/win_temp_count.cmd
+++ labfiles/HOL-2401-02/win_temp_count.cmd
@@ -1,16 +1,17 @@
 @echo off
 setlocal enabledelayedexpansion
 
-set folderpath=%TEMP%
+:: # set folderpath=%TEMP%
+set folderpath=C:\TEMP
+
 set /a count=0
 
-for %%A in ("%folderpath%\*") do (
-    if not exist "%%~fA\" (
-        set /a count=!count!+1
-    )
+for /R "%folderpath%" %%A in (*) do (
+    set /a count=!count!+1
 )
 
 echo !count!
+endlocal
 
 :: # echo Number of files in %folderpath%: !count!
 :: # exit /b !count!
