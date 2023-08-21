a6b7cfc30a1eb6871a85d25ad69c69ea8170e0b5
Christopher Lewis <christopher.lewis@thecloudxpert.net>
2023-08-21 19:30:58 +0100

    UPdates for HOL-2401-01-08 Files

---
+++ labfiles/HOL-2401-08/Module 1/getVmWithPs.ps1
@@ -0,0 +1,11 @@
+# Connect to vCenter
+Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Confirm:$false | Out-Null  2> $null
+Connect-VIserver "vcsa-01a.corp.vmbeans.com" -username "administrator@vsphere.local" -password "VMware1!" -ErrorAction Stop | Out-Null 2> $null
+
+# Search for the VM by name
+$vmPartialName = $inputs.vmName
+Write-Host "Searching VM with name matching $vmPartialName"
+$matchingVms = Get-VM | Where-Object { $_.Name -like "*$vmPartialName*" }
+Write-Host "$($matchingVms.Count) VM(s) found with name matching $vmPartialName"
+
+return $matchingVms[0].Name
